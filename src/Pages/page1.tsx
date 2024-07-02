47cbce2b05ae4a110e7d14183862f717266f73dd
Abdallah Diab <abdallah.diab1999@gmail.com>
2024-07-02 15:31:32 +0300

    feat: use arr in useTrans

--- src/Pages/page1.tsx
+++ src/Pages/page1.tsx
@@ -2,14 +2,15 @@ import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 
 export default function Page1() {
-  const { t } = useTranslation("auth");
+  const { t } = useTranslation(["auth", "home"]);
 
   return (
     <div>
       <Link to="/home">Home</Link>
       <h1>Page1</h1>
-      <p>{t("auth", { context: "key1" })} </p>
-      <p>{t("auth", { context: "user_name" })} </p>
+      {/* <p>{t("auth", { context: "key1" })} </p> */}
+      <p>{t("auth:label", { context: "userName" })} </p>
+      <p>{t("home:label", { context: "test" })} </p>
     </div>
   );
 }
